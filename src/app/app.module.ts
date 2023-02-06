4e8c05bfd2e0d7966ccea688118cad9e701e9607
MichalIzb <michalizb@wp.pl>
2023-02-06 13:42:44 +0100

    component generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyFollowersComponent
+    MyFollowersComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
